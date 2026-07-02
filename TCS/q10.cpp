b3ed8430fd306614f40b70f07444326a13a49f85
BHUMIKA BAHUGUNA <158489676+BhumikaBahuguna@users.noreply.github.com>
2026-07-02 17:58:57 +0530

    Add files via upload

---
+++ TCS/q10.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int sumofdig(int n){
+    int sum=0;
+    while(n>0){
+        int d=n%10;
+        sum+=d;
+        n/=10;
+    }
+    return sum;
+}
+int main(){
+    int n;
+    cin>>n;
+    int r;
+    cin>>r;
+    int s=sumofdig(n);
+    s*=r;
+    s=sumofdig(s);
+    cout<<s<<endl;
+}
