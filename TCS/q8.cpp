e0df315ec16bc8376cf50529ae92469b3e73fce1
BHUMIKA BAHUGUNA <158489676+BhumikaBahuguna@users.noreply.github.com>
2026-07-02 17:35:32 +0530

    Update q8.cpp

--- TCS/q8.cpp
+++ TCS/q8.cpp
@@ -18,3 +18,35 @@ int main(){
     cout<<maxi;
     return 0;
 }
+
+/* 
+avoiding inner loop
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string str;
+    cin >> str;
+
+    int L;
+    cin >> L;
+
+    int cnt = 0;
+    int maxi = 0;
+
+    for(int i = 0; i < str.length(); i++)
+    {
+        if(str[i] == 'a')
+            cnt++;
+
+        if((i + 1) % L == 0 || i == str.length() - 1)
+        {
+            maxi = max(maxi, cnt);
+            cnt = 0;
+        }
+    }
+
+    cout << maxi;
+}
+*/
