b4df3d8df8b66109ed4c75df65ec216711c23edb
BHUMIKA BAHUGUNA <158489676+BhumikaBahuguna@users.noreply.github.com>
2026-07-05 19:43:58 +0530

    Add files via upload

---
+++ TCS/q20.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    vector<int>a(n);
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    int ans=a[n-1];
+    for(int i=1;i<n;i+=2){
+        if(a[i]!=a[i-1]){
+            ans=a[i-1];
+            break;
+        }
+    }
+    cout<<ans<<endl;
+    return 0;
+}
