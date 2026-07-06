b35525ba8f9941830817872708ec0bc82eeeaf2b
BHUMIKA BAHUGUNA <158489676+BhumikaBahuguna@users.noreply.github.com>
2026-07-06 09:43:35 +0530

    Add files via upload

---
+++ TCS/q31.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    vector<int> a(n);
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    int maxi=a[0];
+    int count=1;
+    for(int i=1;i<n;i++){
+        if(a[i]>maxi){
+            count++;
+            maxi=max(maxi,a[i]);
+        }
+    }
+    cout<<count<<endl;
+
+}
